1e8b8e1be2576d8f6da819770c0aabbd8a6282d7
Fredus14 <fabioamaral@me.com>
2014-11-11 15:50:34 +0000

    Fix mistakes and added comments
    Had a problem in makeCacheMatrix function and in the tests of the
    functions
    Added better comments

--- cachematrix.R
+++ cachematrix.R
@@ -9,19 +9,33 @@
 ## 3.   set the value of the Inverse of matrix
 ## 4.   get the value of the Inverse of matrix
 
-makeCacheMatrix <- function(x = matrix()) {
 
-        inv <- "No value has been set for the matrix yet"
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
+makeCacheMatrix <- function(x = matrix()) { # input x will be a matrix
+        
+        m <- NULL    #  m will store our 'inverse' and it's reset to NULL every 
+        #    time makeVCacheMatrix is called
+        
+        set <- function(y) {    # takes an input matrix
+                x <<- y         # saves the input matrix 
+                m <<- NULL      # resets the inverse to NULL, basically what happens when a new object is generated.
         }
-        get <- function() x
-        setinv <- function(solve) inv <<- solve
-        getinv <- function() inv
-        list(set = set, get = get,
-             setinv = setinv,
-             getinv = getinv)
+        
+        #  note these next three functions are not run when makeCacheMatrix is called.
+        #   instead, they will be used by cacheSolve() to get values for x or for
+        #   m (solve) and for setting the inverse.  These are usually called object 'methods'
+        
+        get <- function() { x }   # x$get() this function returns the value of the original matrix
+        
+        setinverse <- function(solve) { #x$setinverse() this is called by cachemean() during the first cacheSolve() access
+                m <<- solve }  #  and it will store the value using superassignment
+        
+        getinverse <- function() { m } # x$getmean this will return the cached value to cacheSolve() on
+        #  subsequent accesses
+        
+        list(set = set,                #   This list is returned with the newly created list object.  
+             get = get,                #   It lists all the functions ("methods") that are part of
+             setinverse = setinverse,  #   the object.  If a function is not on the list then it cannot
+             getinverse = getinverse)  #   be accessed externally.
         
 }
 
@@ -30,27 +44,36 @@ makeCacheMatrix <- function(x = matrix()) {
 #If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should skip the computation 
 #and retrieve the inverse from the cache.
 
+## Usage format: cacheSolve(special_matrix)
 
 cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of matrix 'x'
         
-        inv <- x$getinv()
-        if(!is.null(inv)) {
+        ## Retrieve inverse and assign to m
+        m <- x$getinverse()
+        
+        ## Check if m has a value (i.e. if the inverse has been calculated before),
+        ## indicate if so
+        if(!is.null(m)) {
                 message("getting cached data")
-                return(inv)
+                return(m)
         }
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setinv(inv)
-        inv
+        
+        ## Else, calculate inverse
+        data <- x$get() ##x$get() returns the value of the original matrix and then its assigned to data
+        m <- solve(data, ...) ##solve creates the inverse of original matrix stored in data and asssign it to m
+        
+        ## Store inverse in special_matrix to m in makeCacheMatrix setinverse <- function(solve) { m <<- solve }
+        x$setinverse(m)
+        
+        ## Return m
+        m
 }
 
-
 ## Testing the above functions
+set.seed(1)
 x <- matrix(rnorm(25, 2, 1), 5, 5) ## Create a random normal matrix with 5 rows and 5 collumns and atribute it to variable x
-X<- makeCacheMatrix(x) ## apply the functions above to x and atribute it to the variable X
-X$get() ## Get the matrix x
-X$getinv() ## The inverse matrix has not yet been calculated so it returns the inv value "There is not value for the inverse matrix yet"
-X$setinv(solve(x)) ## Set the inverse of the matrix calculating it.
-X$getinv() ## Return the calculated inverse of the matrix. If this function is called again it will and the matrix has not been changed
-           ## it will skip the calculation and return the cached inverse matrix again.
+X<- makeCacheMatrix(x)
+cacheSolve(X)
+
 
